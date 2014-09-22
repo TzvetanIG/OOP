4ee2ad784a7411064b081fe0bf6180dcda2f55a7
TzvetanIG <cecoig@mail.bg>
2014-09-22 14:54:43 +0300

    Add Homework Exeption-Handling

---
+++ Exeption-Handling/1.SquareRoot/SquareRoot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        string inputString = Console.ReadLine();
+
+        try
+        {
+            int number = int.Parse(inputString);
+            if (number < 0)
+            {
+                throw new ArithmeticException();
+            }
+
+            double squareRoot = Math.Sqrt(number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid nember");
+        }
+        catch
+        {
+            Console.WriteLine("Invalid nember");
+        }
+        finally
+        {
+            Console.WriteLine("Good Bye");
+        }
+    }
+}
+
---
+++ Exeption-Handling/2.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class EnterNumbers
+{
+    static void Main()
+    {
+        int start = 1;
+        int end = 100;
+        int[] numbers = new int[10];
+        bool isInvalid = false;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            do
+            {
+                try
+                {
+                    numbers[i] = ReadNumber(start, end);
+                    isInvalid = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The number must to be in range [" + start + ".." + end + "]");
+                    isInvalid = true;
+                }
+            } while (isInvalid);
+        }
+    }
+
+    public static int ReadNumber(int start, int end)
+    {
+        string numberAsString = Console.ReadLine();
+        int number = int.Parse(numberAsString);
+        if (number < start || number > end)
+        {
+            throw new FormatException("The number must to be in range [" + start + ".." + end + "]");
+        }
+
+        return number;
+    }
+}
+
+
