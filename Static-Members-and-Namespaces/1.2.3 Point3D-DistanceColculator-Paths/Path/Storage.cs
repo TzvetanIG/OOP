57fa0dbb4b3f5c442e6366bbc9446d3c1bf79376
TzvetanIG <cecoig@mail.bg>
2014-09-22 14:12:43 +0300

    Fix project Path

--- Static-Members-and-Namespaces/1.2.3 Point3D-DistanceColculator-Paths/Path/Storage.cs
+++ Static-Members-and-Namespaces/1.2.3 Point3D-DistanceColculator-Paths/Path/Storage.cs
@@ -6,25 +6,40 @@ static class Storage
 {
     public static void SavePathInFile(string fileName, Path path)
     {
-        File.WriteAllText(fileName, path.ToString());
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            writer.Write(path);
+        }
+
     }
 
     public static Path LoadPathOfFile(string fileName)
     {
-        string input = File.ReadAllText(fileName);
-        string pattern = "{([\\d,.]+), ([\\d,.]+), ([\\d,.]+)}";
-
-        var reg = new Regex(pattern);
-        var matchs = reg.Matches(input);
         Path path = new Path();
-        foreach (Match match in matchs)
-        {
-            double x = double.Parse(match.Groups[1].Value);
-            double y = double.Parse(match.Groups[2].Value);
-            double z = double.Parse(match.Groups[3].Value);
 
-            Point p = new Point(x, y, z);
-            path.AddPoint(p);
+        using (StreamReader sr = new StreamReader(fileName))
+        {
+            string input = sr.ReadToEnd();
+
+            string pattern = "{([\\d,.]+), ([\\d,.]+), ([\\d,.]+)}";
+
+            var reg = new Regex(pattern);
+            var matchs = reg.Matches(input);
+
+            if (matchs.Count <= 0)
+            {
+                throw new ApplicationException("Invalid data in file " + fileName);
+            }
+
+            foreach (Match match in matchs)
+            {
+                double x = double.Parse(match.Groups[1].Value);
+                double y = double.Parse(match.Groups[2].Value);
+                double z = double.Parse(match.Groups[3].Value);
+
+                Point p = new Point(x, y, z);
+                path.AddPoint(p);
+            }
         }
 
         return path;
